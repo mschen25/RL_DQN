c5594877cfceba52c1b58bbb29266760606eb16a
mschen25 <646476489@qq.com>
2024-09-29 18:43:59 +0800

    Initial commit

---
+++ Environment.py
@@ -0,0 +1,45 @@
+from MDP_model import MDP
+import numpy as np
+class Environment:
+    def __init__(self):
+        # 初始状态
+        self.ori_state=None
+        # 下一个state
+        self.next_state=None
+        # 此前的奖励积累
+        self.reward=None
+        # 此前的运行状态 true为完成
+        self.done=False
+        #终点
+        self.end = None
+        self.mdp = MDP()
+
+    # 返回初始状态
+    def reset(self):
+        return np.array(self.ori_state)
+
+    # 更新初始状态
+    def make(self,i):
+        self.ori_state=self.mdp.set_state_and_target(i)#更新ori_state
+        self.end = self.mdp.get_end_point()
+
+    def step(self,action):
+        # 更新当前状态和下一状态
+        state, self.next_state=self.mdp.state_transfer(action)
+        # 更新奖励
+        self.reward = self.mdp.get_reword(state,self.next_state)
+
+        # 更新运行状态done
+        if self.next_state[1] < 0 or self.next_state[0] == self.end:
+            self.done=True
+        else:
+            self.done=False
+
+        return np.array(self.next_state), self.reward, self.done
+
+
+if __name__ == "__main__" :
+    env=Environment()
+    env.make(1)
+    a = env.reset()
+    ns,r,d = env.step(26)
---
+++ runTrain.py
@@ -0,0 +1,116 @@
+import gym
+from sympy import false
+from Environment import Environment
+from RL_DQN import DQN, ReplayBuffer
+import torch
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+import numpy as np
+# GPU运算
+device = torch.device("cuda"if torch.cuda.is_available() else "cpu")
+
+# ------------------------------- #
+# 全局变量
+# ------------------------------- #
+
+capacity = 200  # 经验池容量
+lr = 1e-3  # 学习率
+gamma = 0.98  # 折扣因子
+epsilon = 0.8  # 贪心系数
+epsilon_min = 0.1 #最小贪心系数
+epsilon_decay = 0.98 #贪心系数折扣率
+target_update = 5  # 目标网络的参数的更新频率
+batch_size = 5
+n_hidden = 128  # 隐含层神经元个数
+min_size = 10  # 经验池超过多少后再训练
+return_list = []  # 记录每个回合的回报
+loss = []
+Is_train=True  #是否有训练模型
+episode = 1000
+
+
+# 加载环境
+env = Environment()
+n_states = 4  # 4
+n_actions = 50  # 2
+
+# 实例化经验池
+replay_buffer = ReplayBuffer(capacity)
+# 实例化DQN
+agent = DQN(n_states=n_states,
+            n_hidden=n_hidden,
+            n_actions=n_actions,
+            learning_rate=lr,
+            gamma=gamma,
+            epsilon=epsilon,
+            target_update=target_update,
+            device=device,
+            Is_train=Is_train
+            )
+for Id in range(50):
+    env.make(Id)
+    loss = []
+    return_list = []  # 记录每个回合的回报
+    with tqdm(total=episode) as pbar:
+        # 训练模型
+        for i in range(episode):  # 100回合
+            # 每个回合开始前重置环境
+            state = env.reset()  # len=4
+            # 记录每个回合的回报
+            episode_return = 0
+
+            done = False
+
+            while True:
+                # 获取当前状态下需要采取的动作
+                action = agent.take_action(state)
+                # 更新环境
+                next_state, reward, done = env.step(action)
+                # 添加经验池
+                replay_buffer.add(state, action, reward, next_state, done)
+                # 更新当前状态
+                state = next_state
+                # 更新回合回报
+                episode_return += reward
+
+                # 当经验池超过一定数量后，训练网络
+                if replay_buffer.size() > min_size:
+                    # 从经验池中随机抽样作为训练集
+                    s, a, r, ns, d = replay_buffer.sample(batch_size)
+                    # 构造训练集
+                    transition_dict = {
+                        'states': s,
+                        'actions': a,
+                        'next_states': ns,
+                        'rewards': r,
+                        'dones': d,
+                    }
+                    # 网络更新
+                    loss.append(agent.update(transition_dict))
+                # 找到目标就结束
+                if done: break
+            # 记录每个回合的回报
+            return_list.append(episode_return)
+
+            # 更新进度条信息
+            pbar.set_description(desc= "Iteration %d" %Id)
+            pbar.set_postfix({
+                'return': '%.3f' % np.mean(return_list),
+                "loss": '%f' % np.mean(loss) if len(loss) > 0 else 0
+            })
+            pbar.update(1)
+
+            #贪心系数衰减
+            if epsilon > epsilon_min:
+                epsilon *= epsilon_decay
+
+#保存模型
+agent.model_save()
+
+# 绘图
+episodes_list = list(range(len(return_list)))
+plt.plot(episodes_list, return_list)
+plt.xlabel('Episodes')
+plt.ylabel('Returns')
+plt.title('DQN Returns')
+plt.show()
